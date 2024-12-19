8ae779f48d45b2f4720160dd02bedfda6791a196
zain shekhani <157803688+zainshekhani54321@users.noreply.github.com>
2024-12-19 20:36:37 +0500

    Add files via upload

---
+++ todo list/index.js
@@ -0,0 +1,36 @@
+
+// var getul = document.getElementById('ul')
+
+// function foo(){
+//     var a =  document.getElementById('inp').value
+//     var li = document.createElement('li')
+//     var litext = document.createTextNode(a)
+//     li.appendChild(litext)
+//     getul.appendChild(li)
+//     var b = document.getElementById('inp')
+//     b.value = ""
+//    var butt = document.createElement('button')
+//    var buttext = document.createTextNode('delete')
+//    butt.appendChild(buttext)
+//    li.appendChild(butt)
+//    butt.setAttribute('onclick','dell(this)')
+//    var adit = document.createElement('button')
+//    var adittext = document.createTextNode('edit')
+//    adit.appendChild(adittext)
+//    li.appendChild(adit)
+//    adit.setAttribute('onclick','ed(this)')
+
+// }
+
+// function del(){
+//     getul.innerHTML=""
+// }
+// function dell(e){
+//     e.parentNode.remove()
+
+// }
+
+// function ed(c){
+//    var edit = prompt('enter edit value',c.parentNode.firstChild.nodeValue)
+//    c.parentNode.firstChild.nodeValue=edit
+// }
